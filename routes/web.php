76f7ffcec2c3144ff51ea551b233cdca51705742
andreas <andreas.georgopulos@gmail.com>
2018-04-01 06:38:49 +0200

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
 
-$admin = function () {
-    Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
-        Route::get('/', 'IndexController@index')->name('admin-home');
-    });
+$routes = [];
+$routes['admin'] = function () {
+	Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
+		Route::get('/', 'IndexController@index')->name('admin-home');
+	});
 };
 
-$delta_triangle = function() use ($admin) {
-    $admin();
-    Route::group(['namespace' => 'Domains\DeltaTriangle'], function() {
-        Route::get('/', 'IndexController@index')->name('home');
-    });
+$routes['delta_triangle'] = function() use (&$routes) {
+	$routes['admin']();
+	Route::group(['namespace' => 'Domains\DeltaTriangle'], function() {
+		Route::get('/', 'IndexController@index')->name('home');
+	});
 };
 
-$drone_stock = function() use ($admin) {
-    $admin();
-    Route::group(['namespace' => 'Domains\DroneStock'], function() {
-        Route::get('/', 'IndexController@index')->name('home');
-    });
+$routes['drone_stock'] = function() use (&$routes) {
+	$routes['admin']();
+	Route::group(['namespace' => 'Domains\DroneStock'], function() {
+		Route::get('/', 'IndexController@index')->name('home');
+	});
 };
 
-Route::domain('delta-triangle.com')->group($delta_triangle);
-Route::domain('delta-triangle.hu')->group($delta_triangle);
-Route::domain('drone-stock.com')->group($drone_stock);
-Route::domain('drone-stock.hu')->group($drone_stock);
+Route::domain('delta-triangle.com')->group($routes['delta_triangle']);
+Route::domain('delta-triangle.hu')->group($routes['delta_triangle']);
+Route::domain('drone-stock.com')->group($routes['drone_stock']);
+Route::domain('drone-stock.hu')->group($routes['drone_stock']);
